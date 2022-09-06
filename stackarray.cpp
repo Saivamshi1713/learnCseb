e5978a6ffe0df84ca59e22164a9f436c18ee10a6
Saivamshi1713 <98334349+Saivamshi1713@users.noreply.github.com>
2022-09-06 12:02:35 +0530

    Add files via upload

---
+++ stackarray.cpp
@@ -0,0 +1,87 @@
+//C++ program to implement stack operations using Arrays 
+
+#include <iostream>
+using namespace std;
+#define max 50
+class stackk
+{
+    public:
+    //int max=50;
+    int st[max];
+    int top=-1;
+    
+    void push()
+    {
+        int element;
+        cout<<"enter the element to be pushed: ";
+        cin>>element;
+        if(top==max-1)
+        {
+            cout<<"stack is full"<<endl;
+            return;
+        }
+        else
+        {
+            top=top+1;
+            st[top]=element;
+            cout<<"element is pushed"<<endl;
+        }
+        
+    }
+    
+    void pop()
+    {
+        if(top==-1)
+        {
+            cout<<"stack is empty"<<endl;
+            return;
+        }
+        else
+        {
+            cout<<"element popped is "<<st[top];
+            top=top-1;
+        }
+        return;
+    }
+    
+    void display()
+    {
+        cout<<"stack elements are: "<<endl;
+        for(int i=top;i>=0;i--)
+        {
+            cout<<st[i]<<endl;
+        }
+    }
+};
+
+int main()
+{
+    stackk stk;
+    int option;
+    
+    while(1)
+    {
+        cout<<endl<<"Stack Operations"<<endl;
+        cout<<"1. push"<<endl<<"2. pop"<<endl<<"3. Display"<<endl<<"4. exit"<<endl;
+        cout<<"enter the option: ";
+        cin>>option;
+    
+        switch(option)
+        {
+            case 1: 
+                stk.push();
+                break;
+            case 2: 
+                stk.pop();
+                break;
+            case 3:
+                stk.display();
+                break;
+            case 4:
+                exit(1);
+            default:
+                cout<<"wrong entry enter the correct option"<<endl;
+        }
+    }
+    return 0;   
+}
